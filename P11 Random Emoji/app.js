91f6d8946f59538927670314bd83131430d206f6
AbhayJasrotia <137689404+AbhayJasrotia@users.noreply.github.com>
2024-01-29 23:13:26 +0530

    Random Emoji

---
+++ P11 Random Emoji/app.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector("#emoji");
+const emojis = [
+  "😘",
+  "💕",
+  "💋",
+  "❤",
+  "🌹",
+  "😍",
+  "👄",
+  "😻",
+  "👅",
+  "💑"
+];
+
+btn.addEventListener("mouseover", () => {
+  btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+});
